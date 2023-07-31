f286cca6dec57f2da17baf2f45b4121e67e17fee
Jose Almir <joseamfj@outlook.com>
2023-07-31 15:17:57 -0300

    Solve problem using mean function

---
+++ one.java
@@ -0,0 +1,8 @@
+public class one {
+    public static void main(String[] args) {
+        Double[] data = {3.4, 2.5, 4.8, 2.9, 3.6, 2.8, 3.3, 5.6, 3.7, 2.8, 4.4, 4.0, 5.2, 3.0, 4.8};
+        System.out.printf("a) %.3f\n", Utils.mean(data));
+
+    }
+}
+
