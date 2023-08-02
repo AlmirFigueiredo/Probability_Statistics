79064dbacbd9cb4eebcf4405a3242662d43cc146
Jose Almir <joseamfj@outlook.com>
2023-08-02 17:32:42 -0300

    Solve problem 1.6

--- One/utils.py
+++ One/utils.py
@@ -18,3 +18,5 @@ def trimmed_mean(data, trim_perc):
     trim_final = len(data)-quantity_to_exclude
     trimmed_data = data_sorted[trim_init : trim_final]
     return sum(trimmed_data)/len(trimmed_data)
+twenty = [2.07, 2.14, 2.22, 2.03, 2.21, 2.03, 2.05, 2.18, 2.09, 2.14, 2.11, 2.02]
+print(mean(twenty))
